7a49db2cc0b5773d7b64c914d5e5f20662992be1
Tou Xiong <tlxiong.prime@outlook.com>
2021-10-14 10:56:32 -0500

    updated bio page to have input elements

--- src/components/BioPage/BioPage.jsx
+++ src/components/BioPage/BioPage.jsx
@@ -1,6 +1,14 @@
-import { useHistory } from 'react-router-dom'
+import { useHistory } from 'react-router-dom';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 function BioPage(){
+    const [ first_name, setFirstName ] = useState('');
+    const [ last_name, setLastName ] = useState('');
+    const [ username, setUsername ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [ image_url, setImageUrl ] = useState('');
+
     const history = useHistory();
 
     const navBack = () => {
@@ -12,12 +20,47 @@ function BioPage(){
             <button onClick = {navBack}>back</button>
             <h1>BIO PAGE</h1>
             <form>
-                <input />
-                <input />
-                <input />
-                <input />
-                <input />
-                
+                <input 
+                    type="text"
+                    name="firstname"
+                    placeholder="First Name"
+                    value={''}
+                    onChange={(event) => name(params)}
+                />
+
+                <input 
+                    type="text"
+                    name="lastname"
+                    placeholder="Last Name"
+                    value={''}
+                    onChange={(event) => name(params)}
+                />
+
+                <input 
+                    type='text'
+                    name="username"
+                    placeholder="Username"
+                    value={''}
+                    onChange={(event) => name(params)}
+                />
+
+                <input 
+                    type='text'
+                    name="password"
+                    placeholder="Password"
+                    value={''}
+                    onChange={(event) => name(params)}
+                />
+
+                <input 
+                    type='text'
+                    name="url_link"
+                    placeholder="Image URL"
+                    value={''}
+                    onChange={(event) => name(params)}
+                />
+
+                <button>Save</button>
             </form>
             
         </div>
