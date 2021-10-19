cbac30765d67e8729396c89d510226703adb7334
Tou Xiong <tlxiong.prime@outlook.com>
2021-10-19 17:10:18 -0500

    updated root reducer to include ride style reducer

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -3,6 +3,7 @@ import errors from './errors.reducer';
 import user from './user.reducer';
 import locationReducer from './location.reducer';
 import profileReducer from './profile.reducer';
+import rideStyleReducer from './rideStyle.reducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -15,6 +16,7 @@ const rootReducer = combineReducers({
   user, // will have an id and username if someone is logged in
   locationReducer,
   profileReducer,
+  rideStyleReducer,
 });
 
 export default rootReducer;
