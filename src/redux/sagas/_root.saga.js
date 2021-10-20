f7e9c5ef8c1769c0591b7bbce270fc0cb357f284
Tou Xiong <tlxiong.prime@outlook.com>
2021-10-19 23:21:58 -0500

    updated root saga to include status saga

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -5,6 +5,7 @@ import profileSaga from './profile.saga';
 import registrationSaga from './registration.saga';
 import userSaga from './user.saga';
 import rideStyleSaga from './rideStyle.saga';
+import statusSaga from './status.saga';
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -21,5 +22,6 @@ export default function* rootSaga() {
     locationSaga(),
     profileSaga(),
     rideStyleSaga(),
+    statusSaga(),
   ]);
 }
