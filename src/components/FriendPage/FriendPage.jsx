8282e88ebe4429205c03ce0ddcd93b1bea9c40aa
Tou Xiong <tlxiong.prime@outlook.com>
2021-10-22 00:36:29 -0500

    attempting to create table friends list

--- src/components/FriendPage/FriendPage.jsx
+++ src/components/FriendPage/FriendPage.jsx
@@ -24,6 +24,20 @@ function FriendPage() {
         <div>
             <button onClick = {navBack}>back</button>
             <h1>Friends</h1>
+            <table>
+                <thead>
+                <tr>
+                    <th>Active Users</th>
+                    <th>Location</th>
+                </tr>
+                </thead>
+                <tbody>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                </tbody>
+            </table>
             {JSON.stringify(store.profileReducer)}
         </div>
     )
